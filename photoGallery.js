248e8a2856876f94ce9f7cf52394302059979b3d
NBurtsev790231 <nburtsev7922@gmail.com>
2023-01-26 23:54:10 +0300

    photoGallery

---
+++ photoGallery.js
@@ -0,0 +1,10 @@
+'use strict'
+//const box = document.getElementById('box').innerHTML='Let yourself rule the dream';
+
+onmouseover = function changeText() {
+	document.getElementById('box').innerHTML='Let yourself rule <br> the dream';
+}
+onmouseout = function offChangeText() {
+	document.getElementById('box').innerHTML='Range Rover collection';
+}
+
